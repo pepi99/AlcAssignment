231fa5db54b30a41b63787ba2c9193484d72650d
Petar <p.ulev-1@student.tudelft.nl>
2020-08-10 13:38:47 +0200

    Final changes, last solutions work as well, but this is more clear end self-explanatory with additional tests and better example in the main method

--- Google_tests/DatasetTest.cpp
+++ Google_tests/DatasetTest.cpp
@@ -23,28 +23,78 @@ TEST(DatasetTest, testOrder) {
 }
 
 /*
- * Tests for correct implementation of median for dataset with odd size of numbers.
+ * Tests for correct order of elements (stack-like, because fastAdd is true)
  */
-TEST(DatasetTest, oddSizeMedianTest) {
+TEST(DatasetTest, testOrderStackLike) {
+    Dataset ds = Dataset();
+    ds.setFastAdd(true);
+    ds.addNumber(1.0);
+    ds.addNumber(2.0);
+    ds.addNumber(1.1);
+    ASSERT_EQ(2.0, ds.getDataset()[1]); // The second element (with index 1) is 2.0
+}
+
+/*
+ * Tests for correct order of elements (stack-like, because fastAdd is true)
+ */
+TEST(DatasetTest, testOrderBinary) {
+    Dataset ds = Dataset();
+    ds.addNumber(1.0);
+    ds.addNumber(2.0);
+    ds.addNumber(1.1);
+    ASSERT_EQ(1.1, ds.getDataset()[1]); // The second element (with index 1) is 1.1, because the list is sorted
+}
+
+/*
+ * Tests for correct implementation of median for dataset with odd size of numbers (which are unordered).
+ */
+TEST(DatasetTest, oddSizeStackLikeMedianTest) {
     Dataset ds = Dataset();
     ds.addNumber(12.3);
     ds.addNumber(9.0);
     ds.addNumber(20.4);
     ds.addNumber(18.0);
     ds.addNumber(3.14);
-    ASSERT_EQ(12.3, ds.getMedian()); // This can be verified manually or using some website.
+    ASSERT_EQ(12.3, ds.getMedian()); // This can be verified manually or using some website. O(log n)
+}
+
+/*
+ * Tests for correct implementation of median for dataset with odd size of numbers (which are ordered).
+ */
+TEST(DatasetTest, oddSizeBinaryMedianTest) {
+    Dataset ds = Dataset();
+    ds.setFastAdd(false);
+    ds.addNumber(12.3);
+    ds.addNumber(9.0);
+    ds.addNumber(20.4);
+    ds.addNumber(18.0);
+    ds.addNumber(3.14);
+    ASSERT_EQ(12.3, ds.getMedian()); // This can be verified manually or using some website. O(1)
 }
 
 /*
  * Tests for correct implementation of median for dataset with even size of numbers.
  */
-TEST(DatasetTest, evenSizeMedianTest) {
+TEST(DatasetTest, evenSizeStackLikeMedianTest) {
     Dataset ds = Dataset();
     ds.addNumber(18.3);
     ds.addNumber(5.91);
     ds.addNumber(7.0);
     ds.addNumber(8.0);
-    ASSERT_EQ(7.5, ds.getMedian()); // // This can be verified manually or using some website.
+    ASSERT_EQ(7.5, ds.getMedian()); // // This can be verified manually or using some website. O(log n)
+}
+
+/*
+ * Tests for correct implementation of median for dataset with even size of numbers.
+ */
+TEST(DatasetTest, evenSizeBinaryMedianTest) {
+    Dataset ds = Dataset();
+    ds.setFastAdd(false);
+    ds.addNumber(18.3);
+    ds.addNumber(5.91);
+    ds.addNumber(7.0);
+    ds.addNumber(8.0);
+    ASSERT_EQ(7.5, ds.getMedian()); // // This can be verified manually or using some website. O(1)
 }
 
 /*
--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int main() {
     Dataset ds = Dataset();
+    ds.setFastAdd(false); // Don't just push back numbers (like a stack), but place each next number in its position in the list such that it is ordered.
     ds.addNumber(10);
     ds.addNumber(6);
     ds.addNumber(11);
     ds.addNumber(5);
-    cout << ds.getDataset()[0] << " " <<  ds.getDataset()[1] <<" " << ds.getDataset()[2] << " " << ds.getDataset()[3];
+    cout << "List is: " << "[" << ds.getDataset()[0] << ", " <<  ds.getDataset()[1]
+    << ", " << ds.getDataset()[2] << ", " << ds.getDataset()[3] << "]" << endl;
     return 0;
 }
